edc493c38af8012e0840f2e1e8076ebee554f767
xeusteerapat <xeus085@gmail.com>
2021-07-04 20:53:11 +0700

    [EDIT] config indent

---
+++ 03-Collection Types/README.md
@@ -0,0 +1,4 @@
+# Collection Types
+
+## Arrays
+
---
+++ 03-Collection Types/array.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [2]string         //array of size 2
+	a[0] = "Hello"          //Zero index of "a" has "Hello"
+	a[1] = "World"          //1st index of "a" has "World"
+	fmt.Println(a[0], a[1]) // will print Hello World
+	fmt.Println(a)          // will print [Hello World]
+}
